d359e63701c8e305890f9988499551bff864e695
RavenRR <romerorraven@icloud.com>
2022-03-07 16:52:40 -0700

    Update method name/add end of input

--- lib/api.rb
+++ lib/api.rb
@@ -7,11 +7,12 @@ class API
     def get_film_data
         film_hash = HTTParty.get(@url + "/films")
         film_array = film_hash
-        self.new_film_objects(film_array)
+        self.film_objects(film_array)
     end
 
     def film_objects(film_array)
         film_array.each do |film_hash|
             Film.new(film_hash)
+        end
     end
 end
