26c315b5496f1561db7fc5b87e9151adbc506bc1
RavenRR <romerorraven@icloud.com>
2022-03-06 21:44:12 -0700

    Update greeting/Add menu

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,9 +1,20 @@
 class CLI
     def run
         greeting
+        API
+        menu
     end
 
     def greeting
         puts "Salutations fellow spirit, let's explore the films of Studio Ghibli"
+        puts "----------------------------------------------------"
+        puts "Please wait, loading menu...."
+        puts "----------------------------------------------------"
+        sleep(3)
+    end
+
+    def menu
+        puts "There are multitudes of films to see, what will you choose first?"
+        puts "----------------------------------------------------"
     end
 end
