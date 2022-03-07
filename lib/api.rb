66224c4de6a824f7cc2a98d17cc1b624cf6d2b77
RavenRR <romerorraven@icloud.com>
2022-03-07 14:17:19 -0700

    Iterate over film array/Create new film each hash

--- lib/api.rb
+++ lib/api.rb
@@ -3,9 +3,14 @@ class API
     def initialize
         @url = "https://ghibliapi.herokuapp.com"
     end
-    
+
     def get_film_data
         film_hash = HTTParty.get(@url + "/films")
-        binding.pry
+        film_hash
+    end
+
+    def film_objects(film_array)
+        film_array.each do |film_hash|
+            Film.new(film_hash)
     end
 end
