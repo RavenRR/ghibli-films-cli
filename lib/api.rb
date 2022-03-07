eb41ecadf40166a3260d1c88cf7b2f8735ba6ee6
RavenRR <romerorraven@icloud.com>
2022-03-07 12:36:01 -0700

    Add get_film_data instance method/film hash

--- lib/api.rb
+++ lib/api.rb
@@ -3,4 +3,9 @@ class API
     def initialize
         @url = "https://ghibliapi.herokuapp.com"
     end
+    
+    def get_film_data
+        film_hash = HTTParty.get(@url + "/films")
+        binding.pry
+    end
 end
