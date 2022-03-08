a6a17ce4f209d79b3b0c849c062a99b117dad49c
RavenRR <romerorraven@icloud.com>
2022-03-07 18:43:25 -0700

    Iterate through film hash key&value/attr_accessor

--- lib/film.rb
+++ lib/film.rb
@@ -1,3 +1,19 @@
 class Film
     
+    @@all = []
+
+    def initialize(film_hash)
+        film_hash.each do |key, value|
+            self.class.attr_accessor(key)
+            self.send("#{key}=", value)
+        end
+
+        @@all << self
+
+    end
+
+    def self.all
+        @@all
+    end
+    
 end
