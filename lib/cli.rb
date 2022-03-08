5a334c89feaa6eab0aa46b6e880bd0385a41b82b
RavenRR <romerorraven@icloud.com>
2022-03-07 18:33:52 -0700

    Init film obj w/ api data/display_title lists objs

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,7 +1,11 @@
 class CLI
+
+    def initialize
+        API.new.get_film_data
+    end
+
     def run
         greeting
-        API.get_film_data
         menu
     end
 
@@ -16,5 +20,12 @@ class CLI
     def menu
         puts "There are multitudes of films to see, what will you choose first?"
         puts "----------------------------------------------------"
+        display_title
+    end
+
+    def display_title
+        Film.all.each.with_index(1) do |film, index|
+            puts "#{index}. #{film.title}"
+        end
     end
 end
